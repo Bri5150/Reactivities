3ad6eb3afd64c109a8b468d66c0c308fcd3ce419
Bri5150 <briaaan@gmail.com>
2022-03-20 23:22:43 -0500

    Chapter 9

--- frontend/src/app/layout/App.tsx
+++ frontend/src/app/layout/App.tsx
@@ -24,7 +24,6 @@ function App() {
                     <>
                         <NavBar />
                         <Container style={{ marginTop: '7em' }}>
-
                             <Route exact path='/activities' component={ActivityDashboard} />
                             <Route path='/activities/:id' component={ActivityDetails} />
                             <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
